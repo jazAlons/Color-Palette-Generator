8290bc2b350d3a4ce6464fa025da60946879d37d
jazAlonso <jazminalonsobri@gmail.com>
2025-06-02 13:53:58 -0600

    Add palette generator with random hex colors

--- script.js
+++ script.js
@@ -1,2 +1,26 @@
 const generateBtn = document.getElementById("generate-btn")
 const paletteContainer = document.querySelector(".palette-container")
+
+
+generateBtn.addEventListener("click", generatePalette);
+
+function generatePalette(){
+    const colors = [];
+
+    for(let i = 0; i < 5; i++){
+        colors.push(generateRandomColor());
+    }
+}
+
+function generateRandomColor(){
+    const letters = "0123456789ABCDEF"
+    let color = "#"
+
+    for(let i = 0; i < 6; i++){
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+
+    return color
+}
+
+generatePalette();
